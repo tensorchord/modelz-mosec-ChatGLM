78d3e3dcf13b5f5f937b8580e3f597865a02de64
Ce Gao <cegao@tensorchord.ai>
2023-04-30 18:15:30 +0800

    fix: Update
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from typing import List
 
 import torch  # type: ignore
-from diffusers import StableDiffusionPipeline  # type: ignore
+from transformers import AutoTokenizer, AutoModel  # type: ignore
 
 from mosec import Server, Worker, get_logger
 from mosec.mixin import MsgpackMixin
@@ -10,27 +10,20 @@ from mosec.mixin import MsgpackMixin
 logger = get_logger()
 
 
-class StableDiffusion(MsgpackMixin, Worker):
+class ChatGLM(Worker):
     def __init__(self):
-        self.pipe = StableDiffusionPipeline.from_pretrained(
-            "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16
-        )
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.pipe = self.pipe.to(device)
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "THUDM/chatglm-6b", trust_remote_code=True)
+        self.model = AutoModel.from_pretrained(
+            "THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+        self.model = model.eval()
 
-    def forward(self, data: List[str]) -> List[memoryview]:
-        logger.debug("generate images for %s", data)
-        res = self.pipe(data)
-        logger.debug("NSFW: %s", res[1])
-        images = []
-        for img in res[0]:
-            dummy_file = BytesIO()
-            img.save(dummy_file, format="JPEG")
-            images.append(dummy_file.getbuffer())
-        return images
+    def forward(self, data):
+        response = model.chat(tokenizer, data, history=[])
+        return response
 
 
 if __name__ == "__main__":
     server = Server()
-    server.append_worker(StableDiffusion, num=1, max_batch_size=4)
+    server.append_worker(ChatGLM, num=1, max_batch_size=4)
     server.run()
